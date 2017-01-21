a79396608ca9e5fb3571d87657431e0272ec2ba8
Nizar AYED <nizarayed@upgrade-code.org>
2017-01-21 11:31:57 +0000

    Changed Codi Daemon scripts

--- codi.sh
+++ codi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PROG="/opt/appengine/bin/dev_appserver.sh"
-OPT="-p 8888 -a 0.0.0.0 /home/nizarayed/git/appinventor-sources/appinventor/appengine/build/war/"
+OPT="-p 8888 -a 0.0.0.0 /opt/codi-studio/appinventor/appengine/build/war/"
 LOGFILE="/var/log/codid.log"
 /bin/bash -c "exec $PROG $OPT > $LOGFILE 2>&1"
 exit $?
